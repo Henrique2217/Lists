a0d9de9025f292bde0428d850f162d2e3bc35f65
Henrique2217 <htrentin17@gmail.com>
2026-03-24 20:33:05 -0300

    lista de busca

--- minhaLista/App.js
+++ minhaLista/App.js
@@ -1,55 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
-    const [nome, setNome]= useState('');
-    const [minhaLista, setMinhaLista] = useState([]);
+  const[dados, setDados] = useState([]);
+  const[loading, setLoading] = useState(true);
 
-    function adicionarContato(){
+  const buscarDados = async () => {
+    try{
+      const resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const dados = await resposta.json();
+      setDados(dados);
 
-      const novoContato = {
-        id: Math.random().toString(),
-        nome: nome
-      }
-
-      setMinhaLista([novoContato, ...minhaLista ]);
-      setNome('')
+    } catch (erro) {
+      console.error('Erro ao buscar dados: ', erro);
+    } finally {
+      setLoading(false);
     }
-  
-  
-  return (
+
+  }
+
+  useEffect(() => {
+    buscarDados();
+  },[]);
+
+  if (loading){
+    return(
+      <View style={styles.container}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+
+  return(
     <View style={styles.container}>
-
-      <Text>Meus contatos</Text>
-
-      <TextInput
-        placeholder='Informe seu nome'
-        value={nome}
-        onChangeText={setNome}
-      />  
-
-      <Button
-        title='Adicionar'
-        onPress={adicionarContato} />
-      
       <FlatList
-        data={minhaLista}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item, index}) => <Text>{item.nome}</Text>}       
-        />
-
-
+        data={dados}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.body}</Text>
+          </View>  
+      )}
+      />
     </View>
-  );
-}
 
+  );
+
+
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
   },
 });
